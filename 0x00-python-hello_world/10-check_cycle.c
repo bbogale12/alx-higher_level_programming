507b6dede76022ac48f06eab56e38f19793c460a
Bereketab Bogale <bbogale12@gmail.com>
2022-06-28 20:47:24 -0400

    Python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,22 @@
 #include "lists.h"
-
 /**
- * check_cycle - checks singly linked list for backward links
- *
- * @list: point in a singly linked list to begin search for backward links
- *
- * Return: 0 if all links move forward through list, 1 if backward link
- *         is found, or -1 on failure
+ * check_cycle - checks for cycle in a linked list
+ * @list: linked list to check
+ * Return: 1 if cycle, 0 if not
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *base, *test;
+	listint_t *temp1 = NULL, *temp2 = NULL;
 
-	base = list;
-	test = list;
+	temp1 = list;
+	temp2 = list;
 
-	while (base && test && test->next)
+	while (list)
 	{
-		base = base->next;
-		test = test->next->next;
-		if (base == test)
+		temp2 = temp2->next;
+		if (!temp1 || !temp2)
+			return (0);
+		if (temp2 == temp1)
 			return (1);
 	}
 	return (0);
