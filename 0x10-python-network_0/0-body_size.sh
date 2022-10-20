a04dc32051643433c64b97ac459558640bde1f6b
Bereketab Bogale <bbogale12@gmail.com>
2022-10-20 23:08:14 +0300

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request an URL, and displays the size of the body of the response
-curl -s "$1" | wc -c
+# body size
+curl -sI $1 | grep "Content-Length:" | cut -d' ' -f2
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays all HTTP methods the server will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# show all possible methods
+curl -sI -X OPTIONS $1 | grep 'Allow:' | cut -d ' ' -f2-
