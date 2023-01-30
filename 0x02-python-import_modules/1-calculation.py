c8671897d868ac0463b08852b53b5436c3444614
Bereketab Bogale <bbogale12@gmail.com>
2023-01-30 15:15:47 +0300

    python import module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    funcs = [('+', add), ('-', sub), ('*', mul), ('/', div)]
+    for func in funcs:
+        print('{:d} {:s} {:d} = {:d}'.format(a, func[0], b, func[1](a, b)))
